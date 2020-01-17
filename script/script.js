3a09692d0aec6b2c59460ed78313d80ec605efa2
goyalion17 <goyalion17@gmail.com>
2020-01-18 00:51:33 +0200

    day 2

--- script/script.js
+++ script/script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const cartBtn = document.getElementById("cart");
   const wishlistBtn = document.getElementById("wishlist");
   const cart = document.querySelector(".cart");
+  const category = document.querySelector(".category");
 
   //создаём функцию создания карточки товаров
   const createCardGoods = (id, title, price, img) => {
@@ -38,34 +39,66 @@ document.addEventListener("DOMContentLoaded", () => {
     return card;
   };
 
-  //создаём еще одну карточку товара, добавляем ребенка и вызываем функцию создания карточки товаров
-  goodsWrapper.appendChild(
-    createCardGoods(1, "Дартс", 2000, "img/temp/Archer.jpg")
-  );
-  goodsWrapper.appendChild(
-    createCardGoods(2, "Фламинго", 3000, "img/temp/Flamingo.jpg")
-  );
-  goodsWrapper.appendChild(
-    createCardGoods(3, "Носки", 200, "img/temp/Socks.jpg")
-  );
-
   //добавляем корзине товаров стиль '' для закрытия модального окна при условии...
   const closeCart = event => {
     const target = event.target;
 
-    //...target идентичен корзине, поэтому всё, что кликается мимо - не target, и окно будет закрыто; или target содержит класс для закрытия(крестик) модального окна
-    if (target === cart || target.classList.contains("cart-close")) {
+    //...target идентичен корзине, поэтому всё, что кликается мимо - не target, и окно будет закрыто; или target содержит класс для закрытия(крестик) модального окна; или обработка события кнопки ESC(27) клавиатуры
+    if (
+      target === cart ||
+      target.classList.contains("cart-close") ||
+      event.keyCode === 27
+    ) {
       cart.style.display = "";
+      document.removeEventListener("keyup", closeCart);
     }
   };
 
   //добавляем корзине товаров стиль flex для открытия модального окна
-  const openCart = () => {
+  const openCart = event => {
+    //запрещает перезагрузку страницы или переход по ссылке
+    event.preventDefault();
+
     cart.style.display = "flex";
+    document.addEventListener("keyup", closeCart);
+  };
+
+  const renderCard = goods => {
+    goodsWrapper.textContent = "";
+    goods.forEach(({ id, title, price, imgMin }) => {
+      goodsWrapper.append(createCardGoods(id, title, price, imgMin));
+    });
+  };
+
+  const getGoods = (handler, filter) => {
+    fetch("./db/db.json")
+      .then(response => response.json())
+      .then(filter)
+      .then(handler);
+  };
+
+  //рандомный вывод товаров на страницу
+  const randomSort = item => {
+    return item.sort(() => Math.random() - 0.5);
+  };
+
+  const choiceCategory = event => {
+    event.preventDefault();
+    const target = event.target;
+
+    if (target.classList.contains("category-item")) {
+      const category = target.dataset.category;
+      getGoods(renderCard, goods =>
+        goods.filter(item => item.category.includes(category))
+      );
+    }
   };
 
   //подвешиваем слушатель событий для кнопки корзины, чтобы при клике срабатывала функция openCart(выше: которая открывает модальное окно с содержимым корзины)
   cartBtn.addEventListener("click", openCart);
 
   cart.addEventListener("click", closeCart);
+  category.addEventListener("click", choiceCategory);
+
+  getGoods(renderCard, randomSort);
 });
